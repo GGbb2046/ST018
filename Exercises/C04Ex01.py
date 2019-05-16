691b56d7522044e6762bbbb37d84e9b563d52b6d
Galab.Bista <galab.bista@spartans.ut.edu>
2019-05-16 18:13:45 -0400

    Class 4 first 2 exercises

---
+++ Exercises/C04Ex01.py
@@ -0,0 +1,5 @@
+x = int(input("Please enter a whole number "))
+if x%2 == 0:
+    print("You have an even number")
+else:   
+    print("You have an odd number")
---
+++ Exercises/C04Ex02.py
@@ -0,0 +1,20 @@
+x = float(input("Please input the magnitude of the earthquake "))
+if x>=8:
+    print("You have a great earthquake")
+elif x>=7:
+    print("You have a major earthquake")
+elif x>=6:
+    print("You have a strong earthquake")
+elif x>=5:
+    print("You have a moderate earthquake")
+elif x>=4:
+    print("You have a light earthquake")
+elif x>=2:
+    print("You have a minor earthquake")
+elif x>=1:
+    print("You have a micro earthquake")
+else:
+    print("You should'nt bother yourself too much. Keep happy and smiling")
+
+
+
