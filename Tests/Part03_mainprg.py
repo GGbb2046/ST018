7d59920d326283d99606c87e5ea42c19678802d9
Galab.Bista <galab.bista@spartans.ut.edu>
2019-05-20 11:03:43 -0400

    Draft_2

---
+++ Tests/Part03_mainprg.py
@@ -0,0 +1,32 @@
+t=int(input("Please enter the temperature in Fahrenheit: "))
+
+while True:   
+    
+    if t<60:
+        print("FALSE. It is too cold for the squirrels to come out and play at ",t, "F be it any season."  )
+        break
+    
+    elif t>100:
+        print("FALSE. It is too hot for the squirrels to come out and play at ",t, "F be it any season."  )
+        break
+
+    elif t<=90:
+        print("TRUE. It is an optimum temperature for squirrels to come out and play at ",t, "F be it any season")
+        break
+    
+    else:
+        s=input("Please enter T for summer or anything else to indicate other seasons ") 
+        if s.upper()=='T':
+            if (60 <= t <= 100):       
+                print("TRUE. The temperature is optimum at ",t ,"F ", "in summer so the squirrels are playing.")
+                break
+            else:
+                print("FALSE. It is too hot at ",t ,"F ", "in summer so the squirrels are not playing.")
+                break
+        else:
+            if (60 <= t <= 90):       
+                print("TRUE. The temperature is optimum at ",t ,"F ", "not in summer so the squirrels are playing.")
+                break
+            else:
+                print("FALSE. Other than summer, it is too hot at ",t ,"F ", "for squirrels to come out playing.")
+                break
--- Tests/Part03_prgusertest.py
+++ Tests/Part03_prgusertest.py
@@ -1,5 +1,9 @@
+#creating a tuple of possible temperature ranges 
 t=(50,55,59,60,70,80,90,91,100,101,110)
+
+#creating a tuple of boolean for presence or absence of summer
 s=("in summer", "not in summer")
+
 print("We have used the following temperature range for testing the program\n")
 for i in t:
     print (i, end= "  ")
@@ -9,29 +13,30 @@ for i in s:
     print (i)
 print('\n')
 
+#testing the program for each possible combination of temperature and boolean
 for i in t:
     while True:   
         
         if i<60:
-            print("It is too cold for the squirrels to come out and play at ",i, "F be it any season."  )
+            print("FALSE. It is too cold for the squirrels to come out and play at ",i, "F be it any season."  )
             break
         
         elif i>100:
-            print("It is too hot for the squirrels to come out and play at ",i, "F be it any season."  )
+            print("FALSE. It is too hot for the squirrels to come out and play at ",i, "F be it any season."  )
             break
 
         elif i<=90:
-            print("It is an optimum temperature for squirrels to come out and play at ",i, "F be it any season")
+            print("TRUE. It is an optimum temperature for squirrels to come out and play at ",i, "F be it any season.")
             break
         
         else:                                  
                     for j in s:
                     
                         if j=='in summer':
-                            print("The temperature is optimum at ",i ,"F", j,"so the squirrels are playing.")
+                            print("TRUE. The temperature is optimum at ",i ,"F", j,"so the squirrels are playing.")
                             
                         else:
-                            print("It is too hot at ",i ,"F ", j, "so the squirrels are not playing.")
+                            print("FALSE. It is too hot at ",i ,"F ", j, "so the squirrels are not playing.")
                             break
                        
         break
