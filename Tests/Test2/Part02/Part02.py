d81485ca996b6573454267b2adcc1bc1994c14bd
Galab.Bista <galab.bista@spartans.ut.edu>
2019-05-26 20:14:07 -0400

    Part02_final

--- Tests/Test2/Part02/Part02.py
+++ Tests/Test2/Part02/Part02.py
@@ -1,90 +1,116 @@
-import sys
-import requests
-import json
-from astropy.table import Table, Column
-import numpy as np
+import sys #importing command line module for entering arguments
+import requests # importing requests to fetch data from api
+import json # importing a data structure which we will use in storing and analyzing data
+from astropy.table import Table, Column # importing table modules to create user information table
+import numpy as np # importing numpy for number operations
+import matplotlib.pyplot as plt # importing graphics module to plot time series data
 
-
-Requirement = ['Currency Converter', 'Stock Info'] 
+Requirement = ['Currency Converter', 'Stock Info'] # Creating of table to help the user input their command line arguments.
 Argument = ['-c', '-s']
 tab= Table([Requirement, Argument],names = ('Requirement','Argument'))
 
-if len(sys.argv)==1:
+if len(sys.argv)==1:# Informing user that they have to input an argument to proceed
+    print("\n")
     print("Please retry entering one of the following arguments to meet your requirement: ")
     print("\n")  
     print(tab)
     
 
-elif len(sys.argv)>2:
+elif len(sys.argv)>2: # Informing the user that they have entered too many arguments
     print("You have entered multiple arguments. Please retry entering a single argument follwing the program name.\n")
     print(tab)
 
 else:
     while True:
-        if sys.argv[1] == '-c':
-            
+        if sys.argv[1] == '-c': 
+            print("\n")
+            print("This extension of the program enables you to compute the equivalent foreign currency amount for your domestic currency. \n")
             Currency = ['US Dollar', 'Eurpean Euro', 'Japanese Yen', 'British Pound', 'Swiss Franc', 'Canadian Dollar', 'New Zealand dollar','South African Rand'] 
             Ticker = ['USD', 'EUR','JPY','GBP','CHF','CAD','NZD', 'ZAR']
             t= Table([Currency, Ticker],names = ('Currency','Ticker'))
             print(t,"\n")
-            dc=input("Please input your domestic currency ticker in the list provided above or any other of your choice: ")
-            fc = input("Please input your foreign currency ticker in the list provided above or any other of your choice: ")
+            dc=input("Please input your domestic currency ticker from the list provided above or any other of your choice: ")
+            fc = input("Please input the foreign currency ticker from the list provided above or any other of your choice: ")
             a= input("The amount of "+dc + " you want to exchange for "+fc+": ")
+            
             if dc==0 or fc ==0 or a==0:
                 print("Please enter valid input.")
-            else:
+            else:# processing input from the customer and calling data from API
                 c='CURRENCY_EXCHANGE_RATE'
                 api_key = "J42OIFGWMD37J58V" # this is my appid for alpha vanatage
                 base_url = 'https://www.alphavantage.co/query?'
                 full_url = base_url +  "function="+c+'&from_currency='+dc.upper()+'&to_currency='+fc.upper()+'&apikey='+api_key 
-                # print("Here is the full URL being used for the WebAPI call", full_url)
+                # fetching the request and storing the data in memory within a json structure
                 f = requests.get(full_url)
                 json_string = f.text
+                
                 if len(json_string) < 1:
                     print("The API is currently unavailable. Please try again in a few minutes. ")
                     break
                 else: 
                     parsed_json = json.loads(json_string)
-                    #print(parsed_json)
-                    try:
+                    
+                    try: #use of exception handle to throw an error if the user is unable to input valid input for tickers and amounts
                         bidr = parsed_json['Realtime Currency Exchange Rate']['8. Bid Price']
                         askr = parsed_json['Realtime Currency Exchange Rate']['9. Ask Price']
                         br = round(float(bidr),4)
                         ar = round(float(askr),4)
                         sp = round((ar-br),4)
                         t = int(br*round(float(a),2))
-                    except:
-                        print("You have entered invalid arguments. Please retry entering valid arguments")
-
-                    else:
+                    
+                    except: #ser informing the user of their invalid inputs
+                        print("\n")
+                        print("You have entered invalid inputs. Please retry entering valid inputs for currency tickers or amount for conversion or both.")
+                        break
+                    
+                    else:# displaying the amounts to be received after exchange with the quoted spread
                     
                         print("The amount you will receive upon exchange is {:,} ".format(t) +  fc)
                         print("The quoted spread on the exchange is {:.4f} ".format(sp))
+                        print('Have a nice day!')
                         break
 
-        elif sys.argv[1] == '-s':
-            s=input("Please enter the stock ticker: ")
+        elif sys.argv[1] == '-s':# checking for the stock analysis extension of the program
+            print("\n")
+            print("This extension of the program enables you to reflect on the current stock price of your ticker with a daily time series for last 100 days.\n")
+            s=input("Please enter a stock ticker of your choice: ")
             t='TIME_SERIES_DAILY'
             api_key = "J42OIFGWMD37J58V" # this is my appid for alpha vanatage
             base_url = 'https://www.alphavantage.co/query?'
             full_url = base_url +  "function="+t+'&symbol='+s+'&apikey='+api_key+'&datatype=json'
-            # print("Here is the full URL being used for the WebAPI call", full_url)
+            
             f = requests.get(full_url)
             json_string = f.text
                 
-            if len(json_string) < 1:
+            if len(json_string) < 1:# informing the user that the program is unable to call data from the server.
                 print("The API is currently unavailable. Please try again in a few minutes. ")
                 break
             else: 
-                # sd= input("Please enter the starting date in mm/dd/yy format: ")
-                # ed= input("Please enter the ending date in mm/dd/yy format: ")
-                parsed_json = json.loads(json_string)
-                # print(parsed_json)
-                data = parsed_json['Time Series (Daily)'].keys()
-                print(data)
+                parsed_json = json.loads(json_string)# parsing the json structure to fetch keys and values
+                
+                try:
+                    date = [date for date in parsed_json['Time Series (Daily)'].keys()]# creating list of dates 
+                    list.reverse(date)# reversing the order of dates in a chronological order
+                    dict2 = {x:parsed_json['Time Series (Daily)'][x]['4. close'] for x in date}# creating a new dictionary for date keys and stock prices as the values
+                    print("The current price of stock "+s+" is "+ dict2[parsed_json['Meta Data']['3. Last Refreshed']])
+                    
+                    #preparing the coordinates for graph
+                    x = dict2.keys() 
+                    y = [float(y) for y in list(dict2.values())]
+                    plt.figure(figsize=(80,10))#setting the size of the graph
+                    plt.plot(x,y,color ='green', marker = '.')
+                    plt.gca().yaxis.set_major_locator(plt.LinearLocator())# setting the ticks for linearity and aesthetics
+                    plt.gca().xaxis.set_major_locator(plt.LinearLocator())
+                    plt.xlabel('Time')# setting the labels for the axes
+                    plt.ylabel('Price')
+                    plt.show()
+                except:
+                    print("You have entered an invalid ticker for your chosen stock.")#exception throw that the user has input an invalid ticker.
+
+
                 break
 
         else:
-            print("Please enter the correct argument after the program name. \n")
+            print("Please enter the correct argument after the program name. \n")# throw an error that the user did not enter the correct argument from the table. 
             print(tab)
             break
---
+++ Tests/Test2/Part02/Program_Description.md
@@ -0,0 +1,24 @@
+## Program Description
+
+1. This program will enable fetching of currency and stock information from a financial api - Alpha Vantage. The program converts the data into a json structure, perform analysis and spits results of the analysis in a json format, driven by a command line argument.
+
+2. There are two analysis available to the user.
+    - The user may be able to either check the current exchange rate and fetch the amount of domestic currency one could exchange for a input foreign currency with the spread information.
+
+    - or, the user may inquire about the current price of a stock complementing it with a time series plot of the stock prices.
+
+3. The program recapitulates key programming concepts discussed in class:
+    - Command line arguments
+    - If else statements
+    - While loops
+    - For loops
+    - Creating Dictionaries and lists
+    - Fetching data from both the above data structures
+    - Converting one data structure to the next
+    - Using Json file structures
+    - Calling data from API's using unique API code.
+    - Use of operators - arithmetic and logical 
+    - Use of matplotlyb for graphical interface
+    - Exception handling 
+
+I would like to sincerely thank our professor, Dr. Tim Smith for his support and collaboration in making our learning intuitive and enlightening.
